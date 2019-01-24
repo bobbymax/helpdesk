d9ba8a8395a305f65626d34f244671ba152c6ac5
Bobby Ekaro <iambobbymax@gmail.com>
2019-01-24 14:40:24 +0100

    Slight Change

--- app/Permission.php
+++ app/Permission.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Permission extends Model
 {
     protected $fillable = ['name', 'slug'];
+    protected $table = 'permissions';
 
     public function roles()
     {
