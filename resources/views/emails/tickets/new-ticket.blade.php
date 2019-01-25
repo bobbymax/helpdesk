6a60baee636bae0ecc84356fcff0e695898f9564
Bobby Ekaro <iambobbymax@gmail.com>
2019-01-25 15:58:57 +0100

    Email message change

--- resources/views/emails/tickets/new-ticket.blade.php
+++ resources/views/emails/tickets/new-ticket.blade.php
@@ -1,7 +1,9 @@
 @component('mail::message')
 # New Ticket Notice!!
 
-A ticket with code {{ $ticket->ticket_code }} and issue "{{ $ticket->issue }}" under request category {{ $ticket->service->name }} has been created by {{ $ticket->owner->name }} located at {{ $ticket->owner->profile->location->name }} room {{ $ticket->owner->profile->room_no }}.
+A ticket with reference number {{ $ticket->ticket_code }} under request category {{ $ticket->service->name }} has been opened by {{ $ticket->owner->name }} located at {{ $ticket->owner->profile->location->name }} room {{ $ticket->owner->profile->room_no }} for the following issue:
+
+* "{{ $ticket->issue }}"
 
 The priority level of this request is {{ $ticket->priority }} and as such should be attended to with immidiate effect.
 
